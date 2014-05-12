2eaeca6c71d2cdbe0f09c616c3cad3c5c4ed2a17
Kirill Pimenov <kirill@pimenov.cc>
2014-05-12 22:32:29 +0400

    Naive check works

--- lib/api/V1/api_v1.ex
+++ lib/api/V1/api_v1.ex
@@ -3,15 +3,21 @@ defmodule AuthStralia.API.V1 do
     use Elli.Handler
 
     post "/token/new" do
-      "alice@example.com" = req.post_arg("user_id")
-      "Correct password" = req.post_arg("password")
+      if (!check_credentials(req.post_arg("user_id"),req.post_arg("password"))) do
+        {401, [], "Authorization failed"}
+      else
+        data = { sub: "alice@example.com",
+                 iss: "auth.example.com",
+                 jti: "1282423E-D5EE-11E3-B368-4F7D74EB0A54" }
+        expiresIn = 86400
+        {:ok, key} = :application.get_env(:auth_stralia, :jwt_secret)
+        http_ok :ejwt.jwt("HS256",data, expiresIn, key)
+      end
+    end
 
-      data = { sub: "alice@example.com",
-               iss: "auth.example.com",
-               jti: "1282423E-D5EE-11E3-B368-4F7D74EB0A54" }
-      expiresIn = 86400
-      {:ok, key} = :application.get_env(:auth_stralia, :jwt_secret)
-      http_ok :ejwt.jwt("HS256",data, expiresIn, key)
+    #TODO Here goes our database stuff
+    defp check_credentials(user_id, password) do
+      ("alice@example.com" == user_id) and ("Correct password" == password)
     end
   end
 end
