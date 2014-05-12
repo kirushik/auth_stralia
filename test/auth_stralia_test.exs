26dacc1eb15de9f8a9f7410d14f5ed8494ab51a7
Kirill Pimenov <kirill@pimenov.cc>
2014-05-12 18:20:12 +0400

    Now Localhost helper can post params

--- test/auth_stralia_test.exs
+++ test/auth_stralia_test.exs
@@ -3,7 +3,7 @@ defmodule AuthStraliaTest do
   use Localhost
 
   test "Correct JSON web token is returned from /token/new endpoint" do
-    response = post('/token/new')
+    response = post('/token/new', %{:user_id => "alice@example.com", :password => "Correct password"})
     {:ok, key} = :application.get_env(:auth_stralia, :jwt_secret)
     {_claims} = :ejwt.parse_jwt(response, key)
   end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -10,13 +10,16 @@ defmodule Localhost do
         list_to_bitstring response
       end
 
-      def post(relative_path) do
+      def post(relative_path, params) do
+        param_strings = for key <- Map.keys(params), do: "#{key}=#{params[key]}"
+        param_string = Enum.join(param_strings, "&")
+
         {:ok, {{_,200,_},_,response}} = :httpc.request(
           :post, 
           { 'http://localhost:3000/api/'++ unquote(api_version) ++ relative_path, 
             [], 
             'application/x-www-form-urlencoded',
-            ''}, #TODO This is body, goes like 'example=here&foo=bar'
+            param_string},
           [], [])
         list_to_bitstring response
       end
